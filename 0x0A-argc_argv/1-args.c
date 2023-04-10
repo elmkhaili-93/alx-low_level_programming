1f5f5990ff92c475b8315ab33134270ea647d9f7
elmkhaili-93 <elmkhailiabdellah93@gmail.com>
2023-04-10 16:03:13 -0700

    ragc argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - number of arguments
+ * @argc: number of arguments 
+ * @argv: array of arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char* argv[])
+{
+	(void) argv;/*ignore argv*/
+	printf("%d\n", argc - 1);
+	return(0);
+}
