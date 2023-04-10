61f641fef4162db3084bf741ba459330e5d098a5
elmkhaili-93 <elmkhailiabdellah93@gmail.com>
2023-04-10 16:07:34 -0700

    ragc argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,14 +3,14 @@
 
 /**
  * main - number of arguments
- * @argc: number of arguments 
- * @argv: array of arguments
+ *@argc: numbeir of arguments
+ *@argv: array of arguments
  *
  * Return: always 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char argv[])
 {
 	(void) argv;/*ignore argv*/
 	printf("%d\n", argc - 1);
-	return(0);
+	return (0);
 }
